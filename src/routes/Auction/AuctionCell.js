45c775789328c38c3ded14516604046ef48f12c1
Hernan Paez <lhernan.paez@gmail.com>
2017-11-18 21:30:23 -0300

    Changed css

--- src/routes/Auction/AuctionCell.js
+++ src/routes/Auction/AuctionCell.js
@@ -11,7 +11,10 @@ class AuctionCell extends Component {
             <div className="pure-u-1-4">
                 <div className="card">
                     <img className="full-width-image" src="https://k32.kn3.net/taringa/3/2/4/8/6/1//doggystar/5AB.gif"/>
-                    <p>Thirds</p>
+                    <div className="card-text">
+                        <h3>Mi trabajo aqui ha terminado</h3>
+                        <h4>Leonard Nimoy</h4>
+                    </div>
                 </div>
             </div>
         )
