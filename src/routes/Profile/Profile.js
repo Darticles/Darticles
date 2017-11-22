daef44289de314ec3e7607afbbb2c2bd8b2f550d
Fernando Ortiz <ortizfernandomartin@gmail.com>
2017-11-22 19:55:52 -0300

    Working on profile

--- src/routes/Profile/Profile.js
+++ src/routes/Profile/Profile.js
@@ -29,8 +29,18 @@ export default class Profile extends Component {
 
     }
 
+    onTextChanged(key) {
+        return ((event) => {
+            console.log(`Key => ${key}. Text => ${event.target.value}`)
+            this.setState({
+                ...this.state,
+                [key]: event.target.value
+            });
+        })
+    }
+
     render() {
-        const { image } = this.state
+        const { image, firstName, lastName, nickName } = this.state
 
         return (
             <div className="App">
@@ -38,11 +48,11 @@ export default class Profile extends Component {
                 <main className="container">
                     <div className="pure-u-1-5"></div>
                     <div className="pure-u-3-5 pure-form">
-                        <h1>Profile</h1>
+                        <h1>Profile for {firstName} {lastName}</h1>
                         <ImageDropzone onImageSelected={this.onImageSelected.bind(this)} image={image}/>
-                        <input style={{marginTop: "10px"}}className="full-width" placeholder="First Name"/>
-                        <input style={{marginTop: "10px"}} className="full-width" placeholder="Last Name"/>
-                        <input style={{marginTop: "10px"}} className="full-width" placeholder="Nickname"/>
+                        <input style={{marginTop: "10px"}} className="full-width" placeholder="First Name" onChange={this.onTextChanged("firstName").bind(this)}/>
+                        <input style={{marginTop: "10px"}} className="full-width" placeholder="Last Name" onChange={this.onTextChanged("lastName").bind(this)}/>
+                        <input style={{marginTop: "10px"}} className="full-width" placeholder="Nickname" onChange={this.onTextChanged("nickName").bind(this)}/>
                         <button className="pure-button pure-button-primary" style={{marginTop: "1.2em"}} onClick={this.onSubmitPressed.bind(this)}>Submit</button>
                     </div>
                     <div className="pure-u-1-5"></div>
