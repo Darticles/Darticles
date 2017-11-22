3affd3d20f809083adca2d6457c9d0997b09c2c2
Fernando Ortiz <ortizfernandomartin@gmail.com>
2017-11-22 18:44:10 -0300

    Working on test cases

--- test/darticles.js
+++ test/darticles.js
@@ -2,21 +2,16 @@ const Darticles = artifacts.require('./Darticles.sol')
 
 contract('Darticles', (accounts) => {
 
-    it('Should set profile without crashing', async function() {
+    it('Should get profile that has been previously set', async function() {
+        const defaultAccount = accounts[0]
         const darticlesInstance = await Darticles.deployed()
-        const sampleProfileImage = "haaseiondq2189e31"
+        const profileImage = "haaseiondq2189e31"
         const firstName = "Fernando"
         const lastName = "Ortiz"
-        const nickName = "fmo91"
-        await darticlesInstance.setProfile.call(sampleProfileImage, firstName, lastName, nickName, {from: accounts[0]})
+        const nickName = "ferortiz"
+        await darticlesInstance.setProfile.call(profileImage, firstName, lastName, nickName, {from: defaultAccount})
+        const profile = await darticlesInstance.getProfile.call({from: defaultAccount})
         assert.equal(true, true, "It should work")
-    })
-
-    it('Should return a profile', async function() {
-        const darticlesInstance = await Darticles.deployed()
-        const profile = await darticlesInstance.getProfile.call(accounts[0])
-        const profileIsNotNull = profile !== undefined
-        assert.equal(profileIsNotNull, true, "Profile should not be undefined")
     })
 
     it('Should add an artwork without crashing', async function() {
@@ -25,7 +20,31 @@ contract('Darticles', (accounts) => {
         const title = "The best artwork"
         const description = "Digital photography"
         await darticlesInstance.addArtwork(imageLink, title, description, { from: accounts[0] })
+        await darticlesInstance.addArtwork(imageLink, title, description, { from: accounts[0] })
+        await darticlesInstance.addArtwork(imageLink, title, description, { from: accounts[0] })
+        const defaultAccount = accounts[0]
+        const artworkIDs = await darticlesInstance.getPortfolio.call({from: defaultAccount})
+        console.log(`Funca? ids: ${artworkIDs}`)
+        const artworks = await Promise.all(artworkIDs.map((id) => darticlesInstance.artwork.call(id)))\
+        // SON ADDRESS ?? :|
+        console.log(`Artworks: ${artworks.map((artwork) => (artwork[0]))}`)
         assert(true, true, "It should work")
     })
     
 })
+
+async function mineBlock() {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.sendAsync({
+            jsonrpc: "2.0",
+            method: "evm_mine",
+            id: 12345
+          }, function(err, result) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+} 
