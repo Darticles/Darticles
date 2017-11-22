afa3fc23609124d0c0bbb616e745fd1c4b2bf964
Hernan Paez <lhernan.paez@gmail.com>
2017-11-22 19:44:27 -0300

    Working on Add Artwork

--- src/routes/NewArtwork/NewArtwork.js
+++ src/routes/NewArtwork/NewArtwork.js
@@ -1,6 +1,9 @@
 // Libraries
 import React, { Component } from 'react'
 import classnames from 'classnames'
+import Contract from 'truffle-contract'
+
+import Darticles from '../../../build/contracts/Darticles.json'
 
 // Components
 import NavigationBar from '../NavigationBar/NavigationBar'
@@ -12,6 +15,9 @@ import '../../css/open-sans.css'
 import '../../css/pure-min.css'
 import '../css/root.css'
 import './css/NewArtwork.css'
+
+import getWeb3 from '../../utils/getWeb3'
+import promisify from '../../utils/promisify'
 
 var axios = require('axios');
 
@@ -25,8 +31,36 @@ export default class NewArtwork extends Component {
         }
     }
 
-    componentWillMount() {
+    async initialize() {
+        const { web3 } = await getWeb3
+    
+        // Create voting entity from contract abi
+        const darticles = Contract(Darticles)
+        darticles.setProvider(web3.currentProvider)
+    
+        const accounts = await promisify(web3.eth.getAccounts)
+        const defaultAccount = accounts[0]
+    
+        const darticlesInstance = await darticles.deployed()
+    
+        this.setState({
+            ...this.state,
+            web3,
+            defaultAccount,
+            darticlesInstance,
+        })
+    
+        // this.loadAuctions()
+    }
 
+    componentWillMount() {
+        this.initialize()
+        .then(() => { 
+            this.setState({
+                ...this.state,
+                ready : true,
+            })
+        })
     }
 
     onImageSelected(image) {
@@ -41,45 +75,61 @@ export default class NewArtwork extends Component {
         // And then go back to porfolio
         if(this.state.image) {
             // document.location="/portfolio"
-            // axios.post('http://localhost:3000/files', {
-            //     file: this.state.image
-            //   })
-            //   .then(function (response) {
-            //     console.log(response);
-            //   })
-            //   .catch(function (error) {
-            //     console.log(error);
-            //   });      
+            var formData = new FormData();
+            formData.append("file", this.state.image)
+
+            axios.post('http://localhost:3000/files', formData, {
+                headers: {
+                  'Content-Type': 'multipart/form-data'
+                }
+            })
+              .then((response) => {
+                const fileID = response.data[0]
+                if (fileID) {
+                    console.log(fileID) 
+                    this.state.darticlesInstance.addArtwork.call(fileID, "titulo1", "description1", { from : this.state.defaultAccount })           
+                    document.location="/portfolio"
+                }
+              })
+              .catch(function (error) {
+                console.log(error);
+              });      
         }
     }
 
-    render() {
+    getUploadForm() {
         const { image } = this.state
-
+        
         return (
+            <div className="pure-g">
+            <div className="pure-u-1-5"></div>
+            <div className="pure-u-3-5 pure-form">
+                        {/* <legend>A compact inline form</legend> */}
+
+                        <input className="full-width" placeholder="Title"/>
+                        <ImageDropzone onImageSelected={this.onImageSelected.bind(this)} image={image}/>
+                        <textarea className="full-width" rows="4" cols="50" placeholder="Description">
+                        </textarea> 
+                        {/* <p><input className="full-width" placeholder="Description"/></p> */}
+
+                        <p><button className="pure-button pure-button-primary" style={{marginTop: "1.2em"}} onClick={this.onSubmitPressed.bind(this)}>Submit</button></p>
+            </div>
+            <div className="pure-u-1-5"></div>
+        </div>
+        )
+    }
+
+    render() {
+        return(
             <div className="App">
                 <NavigationBar/>
                 <main className="container">
-                <h1>Add new artwork</h1>
-                <div className="pure-g">
-                    <div className="pure-u-1-5"></div>
-                    <div className="pure-u-3-5 pure-form">
-                                {/* <legend>A compact inline form</legend> */}
-
-                                <p><input className="full-width" placeholder="Title"/></p>
-                                <p><ImageDropzone onImageSelected={this.onImageSelected.bind(this)} image={image}/></p>
-                                <textarea className="full-width" rows="4" cols="50" placeholder="Description">
-                                </textarea> 
-                                {/* <p><input className="full-width" placeholder="Description"/></p> */}
-
-                                <p><button className="pure-button pure-button-primary" style={{marginTop: "1.2em"}} onClick={this.onSubmitPressed.bind(this)}>Submit</button></p>
-                    </div>
-                    <div className="pure-u-1-5"></div>
-                </div>
-
+                    <h1>Add new artwork</h1>
+                    { this.state.ready ? this.getUploadForm() : <h1>Loading</h1>}
                 </main>
             </div>
         )
+
     }
 
 }
