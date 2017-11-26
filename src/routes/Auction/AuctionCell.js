d5ae8e990a6b82b7446d24d827a02c83092c875d
Fernando Martin Ortiz <ortizfernandomartin@gmail.com>
2017-11-26 13:06:10 -0300

    Working on auctions

--- src/routes/Auction/AuctionCell.js
+++ src/routes/Auction/AuctionCell.js
@@ -9,6 +9,10 @@ class AuctionCell extends Component {
         super(props)
     }
 
+    onClickDetailsButton() {
+        console.log(`On click!`)
+    }
+
     render() {
         const {owner, artworkID, initialPrice, endTimestamp, auctionState, artwork} = this.props.auction
         const {creator, artworkOwner, imageLink, title, description, state} = artwork
@@ -17,9 +21,10 @@ class AuctionCell extends Component {
             <Col s={3}>
                 <Card className='small'
                     header={<CardTitle image={imageLink}>{title}</CardTitle>}
-                    actions={[<a onClick={() => {
-                    }}>See details</a>]}>
+                    actions={[<a onClick={this.onClickDetailsButton.bind(this)}>See details</a>]} >
+
                     {auctionState}
+
                 </Card>
             </Col>
         )
